786a8a215fa63df63901d2eeb70653bfd2ceb1f7
ibrad3 <bradleytaylorstevenson@gmail.com>
2020-11-30 15:54:28 +0000

    Add files via upload

--- web dev/fireworks/Firework.js
+++ web dev/fireworks/Firework.js
@@ -1,9 +1,8 @@
 class Firework{
   constructor(x,y){
-    this.particleCount = 250;
+    this.particleCount = 500;
     this.particles = [];
-    colormode(HSB);
-    this.color = color(random(360),random(255),255);
+    this.color = color(random(255),random(255),random(255));
     for (let i = 0; i < this.particleCount; i++){
       this.particles[i] = new Particle(x,y);
     }
